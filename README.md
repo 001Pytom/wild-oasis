46c4aaa217be95e0916df86da9a8c441c2bcaa3a
faatihat <tomfaah@gmail.com>
2024-07-02 07:58:19 +0100

    half-done

---
+++ README.md
@@ -0,0 +1,1 @@
+I Built this along side Johnas
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+export const supabaseUrl = "https://wpjiumiedyvgksleanzy.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indwaml1bWllZHl2Z2tzbGVhbnp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTczMzcwOTksImV4cCI6MjAzMjkxMzA5OX0.CWL4xc0BZAy7sVlrqG72yyoz_NuCi9AzchFsizZ3jVU";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
